85169f3a02201c2d0d2d630f03eb9e869b78d47f
dbroque1304 <117909883+dbroque1304@users.noreply.github.com>
2023-12-15 22:33:02 +0100

    Update main.java

--- src/main/java/org/iesvdm/main.java
+++ src/main/java/org/iesvdm/main.java
@@ -3,5 +3,6 @@ package org.iesvdm;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world2!");
     }
 }
